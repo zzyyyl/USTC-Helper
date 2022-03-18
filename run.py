193a7e4685134beaaef7d0fb0a8a7081fae7537e
zzyyyl <zhengyile@mail.ustc.edu.cn>
2022-03-18 15:20:32 +0800

    Modify run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from login import Login
 from report import Report
-
+from apply import Apply
 from config import config
 
 if __name__ == "__main__":
@@ -9,5 +9,8 @@ if __name__ == "__main__":
     # print("service:\n", config["service"])
     service = input("Choose service:")
     login = Login(service=config["service"][service]["login"])
+
     if service == Report.SERVICE_NAME:
         report = Report(session=login.session)
+    if service == Apply.SERVICE_NAME:
+        _apply = Apply(session=login.session)
