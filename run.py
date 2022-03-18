c0d53581f96d54405f190e1c81380d2b4b798675
zzyyyl <zhengyile@mail.ustc.edu.cn>
2022-03-18 15:29:32 +0800

    Modify run.py

--- run.py
+++ run.py
@@ -6,11 +6,16 @@ from config import config
 if __name__ == "__main__":
     for service, detail in config["service"].items():
         print(f"{service}: {detail['url']}")
-    # print("service:\n", config["service"])
-    service = input("Choose service:")
-    login = Login(service=config["service"][service]["login"])
+    while True:
+        service = input("Choose service:")
+        if service not in config["service"]:
+            break
+        login = Login(service=config["service"][service]["login"])
 
-    if service == Report.SERVICE_NAME:
-        report = Report(session=login.session)
-    if service == Apply.SERVICE_NAME:
-        _apply = Apply(session=login.session)
+        if service == Report.SERVICE_NAME:
+            report = Report(session=login.session)
+        elif service == Apply.SERVICE_NAME:
+            _apply = Apply(session=login.session)
+        else:
+            break
+    print("Exit.")
