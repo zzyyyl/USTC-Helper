86b27c4099bb7d955e4387de0a234a377c62bf65
zzyyyl <74587068+zzyyyl@users.noreply.github.com>
2022-04-28 22:14:33 +0800

    feat.优化课表的显示方式，可读性更高，并与`zzyyyl/AmiyaTimePlanner`项目的格式相同

--- USTCHelper/lessontable.py
+++ USTCHelper/lessontable.py
@@ -1,9 +1,11 @@
-from sqlalchemy import false
-from sympy import re
 from .config import config
 from .config import LoadConfig, DumpConfig
 from bs4 import BeautifulSoup
 import json
+import numpy as np
+from datetime import datetime, timedelta, timezone
+
+timeline_t = [('beginTime', datetime), ("endTime", datetime), ("event", dict)]
 
 class ApplyError(Exception):
     def __init__(self, text):
@@ -19,7 +21,21 @@ config["service"][LESSONTABLE_SERVICE_NAME] = {
     "url": "https://jw.ustc.edu.cn/for-std/course-table",
     "exec": "https://jw.ustc.edu.cn/for-std/course-table/get-data"
 }
-
+CLASSTIME = [
+    "7:50-8:35",
+    "8:40-9:25",
+    "9:45-10:30",
+    "10:35-11:20",
+    "11:25-12:10",
+    "14:00-14:45",
+    "14:50-15:35",
+    "15:55-16:40",
+    "16:45-17:30",
+    "17:35-18:20",
+    "19:00-19:45",
+    "19:50-20:35",
+    "20:40-21:25"
+]
 class LessonTable:
     SERVICE_NAME = LESSONTABLE_SERVICE_NAME
     service = config["service"][SERVICE_NAME]
@@ -67,7 +83,7 @@ class LessonTable:
 
         dataId = int(res.text.split("var dataId = ", 1)[1].split(";", 1)[0])
         bizTypeId = int(res.text.split("bizTypeId: ", 1)[1].split(",", 1)[0])
-        res = self.session.get(url=self.service['exec'], params = {
+        res = self.session.get(url=self.service['exec'], params={
             "semesterId": semesterId,
             "bizTypeId": bizTypeId,
             "dataId": dataId,
@@ -75,16 +91,69 @@ class LessonTable:
         context = json.loads(res.text)
         courses = []
         for lesson in context["lessons"]:
-            courses.append(
+            start_date = datetime.strptime(lesson["semester"]["startDate"], "%Y-%m-%d")
+            end_date = datetime.strptime(lesson["semester"]["endDate"], "%Y-%m-%d")
+
+            classes_in_week = []
+            for x in lesson["scheduleGroupStr"].split('\n'):
+                items = x.split(' ')
+                classes_in_week.append({
+                    "weekday": int(items[2].split(':', 1)[1].split('(', 1)[0]),
+                    "classes": [int(xi) for xi in items[2].split('(', 1)[1].split(')', 1)[0].split(',')],
+                    "room": items[1],
+                    "teacher": items[3]
+                })
+
+            if start_date.weekday():
+                start_date = start_date + timedelta(days=7-start_date.weekday())
+            weeks_exact = [start_date + timedelta(days=7*(x-1))
+                for x in lesson["suggestScheduleWeeks"]]
+            classes_exact = []
+            for week_exact in weeks_exact:
+                for item in classes_in_week:
+                    start_time = CLASSTIME[item["classes"][0]-1].split('-')[0]
+                    end_time = CLASSTIME[item["classes"][-1]-1].split('-')[1]
+                    day_exact = week_exact + timedelta(days=item["weekday"] - 1)
+                    classes_exact.append({
+                        "startTime": day_exact.replace(
+                            hour=int(start_time.split(':')[0]),
+                            minute=int(start_time.split(':')[1])),
+                        "endTime": day_exact.replace(
+                            hour=int(end_time.split(':')[0]),
+                            minute=int(end_time.split(':')[1])),
+                        "room": item["room"],
+                        "teacher": item["teacher"]
+                    })
+            courses.extend([
                 {
                     "name": lesson["course"]["nameZh"],
-                    "scheduleGroupStr": [
-                        x.split(' ') for x in lesson["scheduleGroupStr"].split('\n')
-                    ],
-                    "start_date": lesson["semester"]["startDate"],
-                    "end_date": lesson["semester"]["endDate"]
-                }
-            )
-        print(json.dumps(courses, indent=4, ensure_ascii=False))
+                    "room": x["room"],
+                    "teacher": x["teacher"],
+                    "startTime": x["startTime"].strftime("%Y-%m-%d %H:%M:%S"),
+                    "endTime": x["endTime"].strftime("%Y-%m-%d %H:%M:%S")
+                } for x in classes_exact
+            ])
+        coursesList = []
+        for course in courses:
+            coursesList.append((course["startTime"], course["endTime"], json.dumps({
+                "name": course["name"],
+                "room": course["room"],
+                "teacher": course["teacher"]
+            }, ensure_ascii=False)))
+        coursesArray = np.array(coursesList, dtype=timeline_t)
+        coursesArray.sort(axis=0, order=('beginTime', 'endTime'))
+        coursesDict = {}
+        for item in coursesArray:
+            day = item["beginTime"].split(' ')[0]
+            if day not in coursesDict:
+                coursesDict[day] = []
+            event = json.loads(item["event"])
+            coursesDict[day].append({
+                "beginTime": item["beginTime"].split(' ')[1],
+                "endTime": item["endTime"].split(' ')[1],
+                "event": f"{event['name']}, {event['room']}, {event['teacher']}"
+            })
+        # coursesList = coursesArray.tolist()
+        print(json.dumps(coursesDict, indent=4, ensure_ascii=False))
         # print(json.dumps(context["lessons"][0], indent=4, ensure_ascii=False))
 config["service"][LESSONTABLE_SERVICE_NAME]["entry"] = LessonTable
